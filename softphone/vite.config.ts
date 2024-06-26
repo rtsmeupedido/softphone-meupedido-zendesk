0eac3c82951c3c9e59f1a67cb17881947e75727c
DanielSiilva <daniellsousa0614@gmail.com>
2024-06-26 09:33:45 -0300

    feat: limp

--- softphone/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-import injectZafHtmlPluginModule from "@app/zendesk/vite-plugin-inject-zaf-html";
-const { injectZafHtmlPlugin } = injectZafHtmlPluginModule;
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), injectZafHtmlPlugin()],
-});
